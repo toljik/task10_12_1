4eac04845c4b4f8e95d783e5ebc92d248f2551c1
root <nazirfedor@gmail.com>
2018-05-18 17:10:47 +0300

    maybe working

--- task10_12.sh
+++ task10_12.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-d=`dirname $0`
+cd $(dirname $0)
+d=`pwd`
 cd $d
 
 #доп параметры
 . "$d/config"
 
 #обновления и доп пакеты
-apt-get update -y -q > /dev/null
-apt-get install libvirt-bin -y -q> /dev/null
-apt-get install qemu-kvm -y -q> /dev/null
-apt-get install virtinst -y -q> /dev/null
+apt-get -yqq update
+apt-get -yqq install libvirt-bin
+apt-get -yqq install qemu-kvm
+apt-get -yqq install virtinst
 #Доп параметры
 MAC=52:54:00:`(date; cat /proc/interrupts) | md5sum | sed -r 's/^(.{6}).*$/\1/; s/([0-9a-f]{2})/\1:/g; s/:$//;'`
 mkdir networks
@@ -30,29 +30,24 @@ fi
 
 echo "<network>
   <name>$EXTERNAL_NET_NAME</name>
-  <forward mode='nat'>
-    <nat>
-      <port start='1024' end='65535'/>
-    </nat>
-  </forward>
+  <forward mode='nat'/>
   <ip address='$EXTERNAL_NET_HOST_IP' netmask='$EXTERNAL_NET_MASK'>
     <dhcp>
       <range start='$EXTERNAL_NET.2' end='$EXTERNAL_NET.254'/>
-      <host mac='$MAC' name='vm1' ip='$VM1_EXTERNAL_IP'/>
+      <host mac='${MAC}' name='$VM1_NAME' ip='$VM1_EXTERNAL_IP'/>
     </dhcp>
   </ip>
-</network>" >  networks/$EXTERNAL_NET_NAME.xml
+</network>" >  $d/networks/$EXTERNAL_NET_NAME.xml
 
 #xml для internal network
 echo "<network>
          <name>$INTERNAL_NET_NAME</name>
-         <ip address='$INTERNAL_NET_IP' netmask='$INTERNAL_NET_MASK'/>
-</network>" > networks/$INTERNAL_NET_NAME.xml
+</network>" > $d/networks/$INTERNAL_NET_NAME.xml
 
 #xml для management
 echo "<network>
          <name>$MANAGEMENT_NET_NAME</name>
-         <ip address='$MANAGEMENT_NET_IP' mask='$MANAGEMENT_NET_MASK'/>
+         <ip address='$MANAGEMENT_HOST_IP' mask='$MANAGEMENT_NET_MASK'/>
 </network>" > networks/$MANAGEMENT_NET_NAME.xml
 
 
@@ -70,8 +65,9 @@ virsh net-start $MANAGEMENT_NET_NAME
 #meta-data vm1
 echo "instance-id: vm1-toljika
 hostname: $VM1_NAME
- -`cat $SSH_PUB_KEY`
 local-hostname: $VM1_NAME
+public-keys:
+ -`cat $SSH_PUB_KEY`
 network-interfaces: |
   auto $VM1_EXTERNAL_IF
   iface $VM1_EXTERNAL_IF inet dhcp
@@ -79,23 +75,15 @@ network-interfaces: |
   auto $VM1_INTERNAL_IF
   iface $VM1_INTERNAL_IF inet static
   address $VM1_INTERNAL_IP
-  network $INTERNAL_NET_IP
   netmask $INTERNAL_NET_MASK
-  broadcast $INTERNAL_NET.254
-  dns $VM_DNS
-
+  
   auto $VM1_MANAGEMENT_IF 
-  ifcae $VM1_MANAGEMENT_IF inet static
+  iface $VM1_MANAGEMENT_IF inet static
   address $VM1_MANAGEMENT_IP
-  network $MANAGMENT_NET
-  netmask $MANAGEMENT_NET_MASK
-  broadcast $MANAGEMENT_NET.254
-  dns $VM_DNS " > $d/config-drives/$VM1_NAME-config/meta-data
+  netmask $MANAGEMENT_NET_MASK" > $d/config-drives/$VM1_NAME-config/meta-data
 
 #user-data vm1
 echo "#!/bin/bash
-password: qwerty
-chpasswd: { expire:False }
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o $VM1_EXTERNAL_IF -j MASQUERADE
 ip link add $VXLAN_IF type vxlan id $VID remote $VM2_VXLAN_IP local $VM1_VXLAN_IP dstport 4789
@@ -105,7 +93,7 @@ curl -fsSl https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
   'deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable'
-apt-get update
+apt-get update -y
 apt-get install docker-ce -y  " > $d/config-drives/$VM1_NAME-config/user-data
 
 
@@ -114,29 +102,22 @@ apt-get install docker-ce -y  " > $d/config-drives/$VM1_NAME-config/user-data
 echo "instance-id: vm2-toljika
 hostname: $VM2_NAME
 local-hostname: $VM2_NAME
+public-keys:
  -`cat $SSH_PUB_KEY`
 network-interfaces: |
   auto $VM2_INTERNAL_IF
   iface $VM2_INTERNAL_IF inet static
   address $VM2_INTERNAL_IP
-  network $INTERNAL_NET_IP
   netmask $INTERNAL_NET_MASK
-  broadcast $INTERNAL_NET.254
-  dns $VM_DNS
 
   auto $VM2_MANAGEMENT_IF
-  ifcae $VM2_MANAGEMENT_IF inet static
+  iface $VM2_MANAGEMENT_IF inet static
   address $VM2_MANAGEMENT_IP
-  network $MANAGMENT_NET
-  netmask $MANAGEMENT_NET_MASK
-  broadcast $MANAGEMENT_NET.254
-  dns $VM_DNS " > $d/config-drives/$VM2_NAME-config/meta-data
+  netmask $MANAGEMENT_NET_MASK" > $d/config-drives/$VM2_NAME-config/meta-data
 
 
 #user-data vm2
 echo "#!/bin/bash
-password: qwerty
-chpasswd: { expire:False }
 ip link add $VXLAN_IF type vxlan id $VID remote $VM1_VXLAN_IP local $VM2_VXLAN_IP dstport 4789
 ip link set $VXLAN_IF up
 ip addr add $VM2_VXLAN_IP/24 dev $VXLAN_IF
@@ -144,7 +125,7 @@ curl -fsSl https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
   'deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable'
-apt-get update
+apt-get update -y
 apt-get install docker-ce -y  " > $d/config-drives/$VM2_NAME-config/user-data
 
 
@@ -165,9 +146,9 @@ virt-install \
   --os-type=linux --os-variant=ubuntu16.04 \
   --disk path=$VM1_HDD,format=qcow2,bus=virtio,cache=none \
   --disk path=$VM1_CONFIG_ISO,device=cdrom \
-  --network network=$EXTERNAL_NET_NAME,mac=$MAC,source=$VM1_EXTERNAL_IF \
-  --network network=$INTERNAL_NET_NAME,source=$VM1_INTERNAL_IF \
-  --network network=$MANAGEMENT_NET_NAME,source=$VM1_MANAGEMENT_IF \
+  --network network=$EXTERNAL_NET_NAME,mac=${MAC} \
+  --network network=$INTERNAL_NET_NAME \
+  --network network=$MANAGEMENT_NET_NAME \
   --graphics vnc,port=-1 \
   --noautoconsole --virt-type $VM_VIRT_TYPE --import
 
@@ -182,8 +163,8 @@ virt-install \
   --os-type=linux --os-variant=ubuntu16.04 \
   --disk path=$VM2_HDD,format=qcow2,bus=virtio,cache=none \
   --disk path=$VM2_CONFIG_ISO,device=cdrom \
-  --network network=$INTERNAL_NET_NAME,source=$VM2_INTERNAL_IF \
-  --network network=$MANAGEMENT_NET_NAME,source=$VM2_MANAGEMENT_IF \
+  --network network=$INTERNAL_NET_NAME \
+  --network network=$MANAGEMENT_NET_NAME \
   --graphics vnc,port=-1 \
   --noautoconsole --virt-type $VM_VIRT_TYPE --import
 
